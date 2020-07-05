44b8b5a37f624ddefb973d52e7bf91a46518c7df
fondamargarita <fondamargarita081@gmail.com>
2020-07-05 15:55:07 -0500

    javascript

---
+++ hora.js
@@ -0,0 +1,20 @@
+function horaMenu (){
+    var d  = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var x = document.getElementById("demo");
+    var desayuno = document.getElementById("desayuno");
+    var comida = document.getElementById("comida")
+    if (h >= 7 && h <= 13){
+        comida.style.display = "none";
+        desayuno.style.display = "block";
+        
+    } else {
+        desayuno.style.display = "none";
+        comida.style.display = "block";
+        
+
+    }
+    
+}
+    
