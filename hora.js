4d0822d8d3dcc94f4d81941818f5215c2204859f
fondamargarita <fondamargarita081@gmail.com>
2020-07-06 00:05:26 -0500

    css update

--- hora.js
+++ hora.js
@@ -1,19 +1,17 @@
 function horaMenu (){
     var d  = new Date();
     var h = d.getHours();
-    var x = document.getElementById("demo");
     var desayuno = document.getElementById("desayuno");
     var comida = document.getElementById("comida")
     if (h >= 7 && h < 13){
         comida.style.display = "none";
         desayuno.style.display = "block";
-        x.innerHTML = "desayuno";
         console.log('hola');
         
     } else {
         desayuno.style.display = "none";
         comida.style.display = "block";
-        x.innerHTML = "comida";
+        
 
     }
     
