4148dca2fdc08149400abbb5423e372bc2b881e0
fondamargarita <fondamargarita081@gmail.com>
2020-07-05 19:40:56 -0500

    update js 3

--- hora.js
+++ hora.js
@@ -1,7 +1,6 @@
 function horaMenu (){
     var d  = new Date();
     var h = d.getHours();
-    var m = d.getMinutes();
     var x = document.getElementById("demo");
     var desayuno = document.getElementById("desayuno");
     var comida = document.getElementById("comida")
